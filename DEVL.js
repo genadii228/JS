5752b025ca8aa92fcfbd6e198bf5665aa66878a7
DEVIL <Andreyxxl7@yandex.ru>
2022-03-07 14:34:58 +0700

    Risynok_4

--- DEVL.js
+++ DEVL.js
@@ -1,10 +1,7 @@
 function draw(){
 	let canvas = document.getElementById('canva');
 	let ctx = canvas.getContext('2d');
-	    ctx.beginPath();
-	    ctx.fillStyle = 'pink';
-  		ctx.moveTo(225,150);
-	  	ctx.lineTo(75,100);
-	  	ctx.lineTo(200,50);
-	 	ctx.fill();
+	    ctx.lineWidth = 5;
+	    ctx.arc(200,200,75,0, 2*Math.PI,true);
+   		ctx.stroke();
 }
