ba7ab906fe51b9f4f31d89b3ce6e74339f82ea9f
DEVIL <Andreyxxl7@yandex.ru>
2022-03-07 15:10:05 +0700

    Risynok_5

--- DEVL.js
+++ DEVL.js
@@ -1,7 +1,15 @@
 function draw(){
 	let canvas = document.getElementById('canva');
 	let ctx = canvas.getContext('2d');
-	    ctx.lineWidth = 5;
-	    ctx.arc(200,200,75,0, 2*Math.PI,true);
-   		ctx.stroke();
+	    
+		ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = "30";
+ctx.moveTo(50,150);
+ctx.lineTo(150,50);
+ctx.lineTo(200,150);
+ctx.lineTo(50,150);
+ctx.lineCap = "round";
+
+ctx.stroke();  
 }
